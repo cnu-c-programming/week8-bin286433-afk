e2ed8295a1931e7bb8f4e0c5d4441fefc3910e9a
bin286433-afk <bin286433@gmail.com>
2026-04-24 10:01:21 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
 
   for(int i=1; i<argc-1; i++){
     for(int j=1; j<argc-1-(i-1); j++){
-      if(strcmp(argv[j], argv[j+1] >0){
+      if( strcmp(argv[j], argv[j+1]) > 0){
         temp=argv[j];
         argv[j] = argv[j+1];
         argv[j+1] = temp;
